79680d1e7102e4c7bfd0bf4385888925d54395fc
elfadili-ae <elfadili.ae@gmail.com>
2023-07-29 13:50:05 +0100

    Fix: delay print and exit on 0

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 int infinite_while(void);
 
@@ -15,13 +17,18 @@ int main(void)
 	while (i < 5)
 	{
 		pid = fork();
-		if (pid != 0)
+		if (pid > 0)
 		{
 			printf("Zombie process created, PID: %d\n", pid);
-			infinite_while();
+			sleep(1);
+			i++;
 		}
-		i++;
+		else
+		{
+			exit (0);
+		}
 	}
+	infinite_while();
 	return (0);
 }
 
