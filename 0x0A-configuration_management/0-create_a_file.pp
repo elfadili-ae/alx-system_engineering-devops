37f00401b7a8f937e346f0e8f3caabef7836db7a
elfadili-ae <elfadili.ae@gmail.com>
2023-09-22 11:06:22 +0100

    Fix: removed path attribute and added group attr

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# Creating school file
+
 file { '/tmp/school':
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
